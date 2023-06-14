15a9e6d713faf98c6890c96bc28b6b72c4809b8c
enzo-viguier <enzoviguier@outlook.fr>
2023-06-15 01:23:42 +0200

    Stripe avec les frais de livraison

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name')
+                ->setFormTypeOptions(['required' => true]),
+            ImageField::new('illustration')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads/')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false)
+                ->setFormTypeOptions(['required' => true]),
+            TextField::new('subtitle')
+                ->setFormTypeOptions(['required' => true]),
+            TextareaField::new('description')
+                ->setFormTypeOptions(['required' => true]),
+            MoneyField::new('price')->setCurrency('EUR')
+                ->setFormTypeOptions(['required' => true]),
+            AssociationField::new('category')
+                ->setFormTypeOptions(['required' => true])
+        ];
+    }
+
+}
---
+++ src/Controller/StripeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
+use Doctrine\ORM\EntityManagerInterface;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StripeController extends AbstractController
+{
+    /**
+     * @Route("/commande/create-session/{reference}", name="app_stripe_create_session")
+     */
+    public function index(EntityManagerInterface $entityManager, Cart $cart, $reference): Response
+    {
+
+        $product_for_price = [];
+        $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+
+        $order = $entityManager->getRepository(Order::class)->findOneByReference($reference);
+
+        if (!$order) {
+            return $this->redirect("app_order", 'error');
+        }
+
+        $product_for_stripe = [];
+
+        foreach ($cart->getFull() as $product) {
+
+            $product_for_stripe[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $product['product']->getPrice(),
+                    'product_data' => [
+                        'name' => $product['product']->getName(),
+                        'images' => [$YOUR_DOMAIN."/uploads/".$product['product']->getIllustration()]
+                    ]
+                ],
+                'quantity' => $product['quantity'],
+            ];
+
+        }
+
+        $product_for_stripe[] = [
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => floatval($order->getCarrierPrice()*100),
+                'product_data' => [
+                    'name' => $order->getCarrierName(),
+                ],
+            ],
+            'quantity' => 1,
+        ];
+
+
+        Stripe::setApiKey('sk_test_51NGTNNClf0DVFyKOZY25mj1qJcSgBWIql3WcGKPdlT5ry7rIQHbE4zEFesjUIojnwAcJwDG8eYH1NCKwfyAYEq1M00gZi6vR5G');
+
+        $checkout_session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                $product_for_stripe,
+            ],
+            'mode' => 'payment',
+            'success_url' => $YOUR_DOMAIN . '/success.html',
+            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+        ]);
+
+//         $response = new JsonResponse(['id' => $checkout_session->id]);
+//         return $response;
+
+        return $this->redirect($checkout_session->url);
+
+    }
+}
