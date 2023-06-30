ca4bf4f0e6bc13f1a7c0b5a6f3bebf507fbe103f
enzo-viguier <enzoviguier@outlook.fr>
2023-06-30 17:57:19 +0200

    Ajout de la dernière version sur github

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -4,10 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Order;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -21,7 +24,11 @@ class OrderCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
     {
         return $actions
-            ->add('index', 'detail');
+            ->add('index', 'detail')
+            ->disable('delete')
+            ->disable('edit')
+            ->disable('new') // Retire le bouton de création d'une commande
+            ;
     }
 
     /*
@@ -35,14 +42,22 @@ class OrderCrudController extends AbstractCrudController
     }
     */
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setDefaultSort(['id' => 'DESC'])->showEntityActionsInlined();
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
             DateTimeField::new('createdAt', 'Passé le'),
-            TextField::new('user.getFullName', 'Utilisateur'),
-            MoneyField::new('total')->setCurrency('EUR'),
-            BooleanField::new('isPaid', 'Payée')
+            TextField::new('user.getFullName', 'Utilisateur')->hideWhenCreating(),
+            MoneyField::new('total', 'Total produit')->setCurrency('EUR'),
+            TextField::new('carrierName', "Transporteur"),
+            MoneyField::new('carrierPrice', 'Frais de port')->setCurrency('EUR'),
+            BooleanField::new('isPaid', 'Payée'),
+            ArrayField::new('orderDetails', 'Produits achetés')->hideOnIndex()
         ];
     }
 
--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -51,7 +51,7 @@ class StripeController extends AbstractController
         $product_for_stripe[] = [
             'price_data' => [
                 'currency' => 'eur',
-                'unit_amount' => floatval($order->getCarrierPrice()*100),
+                'unit_amount' => floatval($order->getCarrierPrice()),
                 'product_data' => [
                     'name' => $order->getCarrierName(),
                 ],
@@ -67,10 +67,12 @@ class StripeController extends AbstractController
                 $product_for_stripe,
             ],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $YOUR_DOMAIN . '/commande/merci/{CHECKOUT_SESSION_ID}',
+            'cancel_url' => $YOUR_DOMAIN . '/commande/erreur/{CHECKOUT_SESSION_ID}',
         ]);
 
+        $order->setStripeSessionId($checkout_session->id);
+        $entityManager->flush();
 
         return $this->redirect($checkout_session->url);
 
