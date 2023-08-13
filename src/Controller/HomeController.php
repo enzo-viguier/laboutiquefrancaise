56218affc3f7e9fa1e514867998d459fae0478f4
OniLinkExe <enzoviguier@outlook.fr>
2023-08-13 03:21:16 +0200

    Reduction du code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
 
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
 
     }
 }
